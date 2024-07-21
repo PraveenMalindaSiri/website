7e15dcdde0ef526c7f08a059933c40e894fab8c3
PraveenMalindaSiri <smpmsiri23@gmail.com>
2024-07-21 17:11:44 +0530

    order details. payment detail input and validation. thanking message. ADDED

--- JS/main2.js
+++ JS/main2.js
@@ -17,3 +17,64 @@ function orderDetailsPay() {
 }
 orderDetailsPay();
 
+const paySubmit = document.getElementById("pay_btn");
+
+function thank(event) {
+    event.preventDefault();
+
+    const name = document.getElementById("c_name").value;
+    const address = document.getElementById("c_address").value;
+    const mobile = document.getElementById("c_pnum").value;
+    const email = document.getElementById("c_email").value;
+    const radios = document.getElementsByName("f");
+
+    const card = document.getElementById("c_cnum").value;
+    const cOwner = document.getElementById("c_cname").value;
+    const cExDate = document.getElementById("c_exd").value;
+    const secNum = document.getElementById("c_csecnum").value;
+    const tOutput = document.getElementById("output_pay");
+
+    let radioValue;
+    for(let i = 0; i < radios.length; i++){
+        if(radios[i].checked){
+            radioValue = radios[i].value;
+            break;
+        }
+    }
+
+    let date = new Date();
+    let orderdate = date.getDate().toString().padStart(2, "0");
+    let orderYear = date.getFullYear();
+    let orderMonth = (date.getMonth() + 1).toString().padStart(2, "0");
+
+    let OrderDate = `${orderYear}-${orderMonth}-${orderdate}`;
+    // console.log(OrderDate);
+
+    let Ddate = new Date();
+    Ddate.setDate(date.getDate() + 7)
+    let Dday = Ddate.getDate().toString().padStart(2, "0");
+    let DYear = Ddate.getFullYear();
+    let Dmonth = (Ddate.getMonth() + 1).toString().padStart(2, "0");
+
+    let DeliverDate = `${DYear}-${Dmonth}-${Dday}`
+
+    if((!card || !cExDate || !cOwner || !secNum)){
+        tOutput.textContent = `Please fill the card detals fields`
+    }
+    else if((!mobile || !email || !name || !address)){
+        tOutput.textContent = `Please fill the fields mobile number and email Customer name and the address.`
+    }
+    else if(!radioValue){
+        tOutput.textContent = `Please select a Payment Method`
+    }
+    else{
+        tOutput.textContent = `Your Order Is done. Customer name ${name}, 
+        your address is "${address}". Payment method is
+        ${radioValue}.
+        Order Date:${OrderDate}.
+        Delivery Date:${DeliverDate}. Thank you for your Order. 
+        (Expect additional information about your order soon.)`
+    }
+
+}
+paySubmit.addEventListener("click", thank);
